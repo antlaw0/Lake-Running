1c00b6fd978dceb8e7a3e3f5a7531a0c9109d781
AAA <aaa@aaa.com>
2017-04-27 16:52:57 -0500

    hope this works

--- hbshelpers/helpers.js
+++ hbshelpers/helpers.js
@@ -1,13 +1,18 @@
 var moment = require('moment');
+var Lake = require('../models/lake');
 
 function dateFormat(date) {
   m = moment.utc(date);
   return m.parseZone().format("dddd, MMMM Do YYYY, h:mm a");
 }
 
+function runTimeTable(lake){
+	return lake.timesRun;
+}
+
 var helpers = {
-  dateFormatter : dateFormat
-  //RunTimeTable : ";
+  dateFormatter : dateFormat,
+  runTimeTable : runTimeTable
 };
 
 module.exports = helpers;
